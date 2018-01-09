d08c0ca0d6f749fccef67bfc362e815428bfa7ff
Suman Hiremath <suman.hiremath89@gmail.com>
2018-01-09 17:53:48 +0530

    first working version for integers

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-var arr = [], result = 0;
-function compute(val){
-  $('#display-screen').text(val);
+var arr = [],
+  result = 0;
 
-  arr.push(val);
-  // if(val == "="){
-  //   result = str;
-  // }
-  if(val == "="){
-    for(var i=0; i<arr.length; i++){
+function compute(val) {
+
+  if (val == "AC" || val == "CE") {
+    $('#display-screen').text("");
+  } else {
+    $('#display-screen').text(val);
+    arr.push(val);
+  }
+  if (val == "=") {
+    //moving elements of the array to a string "result"
+    for (var i = 0; i < arr.length - 1; i++) {
       console.log(arr[i]);
       result += arr[i]
     }
-    console.log("This is the result:" + result);
+    //call different functions depending on floating point or integer
+    if (result.indexOf("/") >= 0 || result.indexOf(".") >= 0) {
+      calculateDecimal(result);
+    } else {
+      calculateInteger(result);
+    }
   }
+}
+
+function calculateDecimal(result) {
+  console.log("This is for decimal");
+}
+
+function calculateInteger(result) {
+
+  var finalResult = eval(result);
+  console.log("This is the result:" + eval(result));
+
+  $('#display-screen').text(result + " = " + finalResult);
+  arr.length = 0;
+  result = 0;
 }
