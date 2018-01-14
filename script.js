9af69d729498f52eba2fcfccb47528130eaec393
Suman Hiremath <suman.hiremath89@gmail.com>
2018-01-14 08:24:45 +0530

    Fixed css, added a header, added continued calc feature

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var arr = [], result = "", interim = 0;
+var arr = [], result = "", interim = 0, resultOnce = false, finalResult = 0, tempArr = [];
 function compute(val){
   // All clear
   if(val == "AC"){
@@ -18,24 +18,39 @@ function compute(val){
   }
   //display the final elements to be calculated on screen
   else {
-    interim = arr.join("");
-    interim += val;
-    console.log(interim);
-    $('#display-screen').text(interim);
-    arr.push(val);
+    if(resultOnce & (val == "/" || val == "*"|| val == "+"|| val == "-")){
+      // interim += finalResult;
+      // console.log("interim value 1" + interim);
+      //
+      // interim += val;
+      // console.log("interim value 2" + interim);
+      arr.push(val);
+      tempArr.push(finalResult);
+      interim = tempArr.join("");
+      interim += val;
+      $('#display-screen').text(interim);
+      tempArr.push(val);
+      resultOnce = false;
+    }
+    else {
+      interim = arr.join("");
+      interim += val;
+      $('#display-screen').text(interim);
+      arr.push(val);
+    }
+
   }
 
   //calculation
   if(val == "="){
-    console.log("This is the array" + arr);
+    console.log("This is the array inside result part" + arr);
     for(var i=0; i<arr.length-1; i++){
       result += arr[i];
-      console.log("This is the result:" + result);
     }
-
-    console.log(eval(result).toFixed(2));
-    $('#display-screen').text(result + " = " + eval(result).toFixed(2));
+    finalResult = eval(result).toFixed(2);
+    $('#display-screen').text(result + " = " + finalResult);
     arr.length = 0;
+    resultOnce = true;
     result = "";
     interim = 0;
   }
